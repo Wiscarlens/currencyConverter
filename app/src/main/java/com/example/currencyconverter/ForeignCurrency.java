9916985143a748d4061358278f32951205ebec49
Wiscarlens <wiscarlens@gmail.com>
2023-10-02 21:02:51 -0400

    set click listener

--- app/src/main/java/com/example/currencyconverter/ForeignCurrency.java
+++ app/src/main/java/com/example/currencyconverter/ForeignCurrency.java
@@ -2,57 +2,89 @@ package com.example.currencyconverter;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ForeignCurrency#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ForeignCurrency extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ForeignCurrency() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ForeignCurrency.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ForeignCurrency newInstance(String param1, String param2) {
-        ForeignCurrency fragment = new ForeignCurrency();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView amount = view.findViewById(R.id.foreign_amountTV);
+        TextView one = view.findViewById(R.id.foreign_currency_one);
+        TextView two = view.findViewById(R.id.foreign_currency_two);
+        TextView three = view.findViewById(R.id.foreign_currency_three);
+        TextView four = view.findViewById(R.id.foreign_currency_four);
+        TextView five = view.findViewById(R.id.foreign_currency_five);
+        TextView six = view.findViewById(R.id.foreign_currency_six);
+        TextView seven = view.findViewById(R.id.foreign_currency_seven);
+        TextView eight = view.findViewById(R.id.foreign_currency_eight);
+        TextView nine = view.findViewById(R.id.foreign_currency_nine);
+        TextView zero = view.findViewById(R.id.foreign_currency_zero);
+        TextView decimal = view.findViewById(R.id.foreign_currency_decimal);
+        TextView check = view.findViewById(R.id.foreign_currency_check);
+
+
+        one.setOnClickListener(v -> {
+
+
+            //amount.setText(updateAmount(amount.getText().toString(), 1));
+            amount.setText(amount.getText() + "1");
+        });
+
+        two.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "2");
+        });
+
+        three.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "3");
+        });
+
+        four.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "4");
+        });
+
+        five.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "5");
+        });
+
+        six.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "6");
+        });
+
+        seven.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "7");
+        });
+
+        eight.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "8");
+        });
+
+        nine.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "9");
+        });
+
+        zero.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "0");
+        });
+
+        decimal.setOnClickListener(v -> {
+            amount.setText(amount.getText() + ".");
+        });
+
+        check.setOnClickListener(v -> {
+            Toast.makeText(getContext(), "Check", Toast.LENGTH_SHORT).show();
+        });
+
     }
 
     @Override
--- app/src/main/java/com/example/currencyconverter/LocalCurrencyFragment.java
+++ app/src/main/java/com/example/currencyconverter/LocalCurrencyFragment.java
@@ -2,11 +2,15 @@ package com.example.currencyconverter;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LocalCurrencyFragment extends Fragment {
 
@@ -19,9 +23,106 @@ public class LocalCurrencyFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView amount = view.findViewById(R.id.local_currency_amount);
+        TextView one = view.findViewById(R.id.local_currency_one);
+        TextView two = view.findViewById(R.id.local_currency_two);
+        TextView three = view.findViewById(R.id.local_currency_three);
+        TextView four = view.findViewById(R.id.local_currency_four);
+        TextView five = view.findViewById(R.id.local_currency_five);
+        TextView six = view.findViewById(R.id.local_currency_six);
+        TextView seven = view.findViewById(R.id.local_currency_seven);
+        TextView eight = view.findViewById(R.id.local_currency_eight);
+        TextView nine = view.findViewById(R.id.local_currency_nine);
+        TextView zero = view.findViewById(R.id.local_currency_zero);
+        TextView decimal = view.findViewById(R.id.local_currency_decimal);
+        TextView check = view.findViewById(R.id.local_currency_check);
+
+        
+        one.setOnClickListener(v -> {
+
+
+            //amount.setText(updateAmount(amount.getText().toString(), 1));
+            amount.setText(amount.getText() + "1");
+        });
+        
+        two.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "2");
+        });
+        
+        three.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "3");
+        });
+        
+        four.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "4");
+        });
+        
+        five.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "5");
+        });
+        
+        six.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "6");
+        });
+        
+        seven.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "7");
+        });
+        
+        eight.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "8");
+        });
+        
+        nine.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "9");
+        });
+        
+        zero.setOnClickListener(v -> {
+            amount.setText(amount.getText() + "0");
+        });
+        
+        decimal.setOnClickListener(v -> {
+            amount.setText(amount.getText() + ".");
+        });
+        
+        check.setOnClickListener(v -> {
+            Toast.makeText(getContext(), "Check", Toast.LENGTH_SHORT).show();
+        });
+
+
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_local_currency, container, false);
+    }
+
+    public static String updateAmount(String amount, int intValue) {
+        // Split the input amount into the integer and fractional parts
+        String[] parts = amount.split("\\.");
+
+        // Extract the integer and fractional parts
+        String integerPart = parts[0];
+        String fractionalPart = parts[1];
+
+        // Convert the fractional part to an integer
+        int fractionalValue = Integer.parseInt(fractionalPart);
+
+        // Update the fractional value based on the input integer
+        fractionalValue += intValue;
+
+        // Ensure the fractional value is within the range [0, 99]
+        fractionalValue = Math.min(99, Math.max(0, fractionalValue));
+
+        // Convert the updated fractional value back to a string
+        fractionalPart = String.format("%02d", fractionalValue);
+
+        // Update the amount with the new fractional part
+        return integerPart + "." + fractionalPart;
     }
 }
